aa497073964ec1d89d5a15135427b93c64e38796
johnwilly990 <j.kim1940@gmail.com>
2023-01-04 22:30:11 -0500

    Create landing page and import nav bar component

---
+++ src/pages/LandingPage.js
@@ -0,0 +1,14 @@
+import NavBar from "../components/NavBar";
+
+const LandingPage = () => {
+  return (
+    <>
+      <NavBar />
+      <div>
+        dakdjfl;kajdk;lfjal;ksdjfl;kajd;klfjaslk;dfjkl;asjf;klasjd;lfkjaskl;d
+      </div>
+    </>
+  );
+};
+
+export default LandingPage;
