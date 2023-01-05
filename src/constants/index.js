b77479ac4454c674298287237b2dabb9b3953de2
johnwilly990 <j.kim1940@gmail.com>
2023-01-05 16:24:23 -0500

    Create constant object for hero description

---
+++ src/constants/index.js
@@ -0,0 +1,6 @@
+export const heroValues = {
+  title: "Welcome to ExTrack!",
+  description:
+    "ExTrack is a free expense tracking application. ExTrack allows you to enter and track your expenses in real-time, categorize them, and generate reports to understand their spending patterns. We aim to help you become more financially repsonsible, and viable, considering the current inflated market and economy!",
+  cta: "Sign Up for Free!",
+};
