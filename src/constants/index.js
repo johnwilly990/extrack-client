f883a34a0a8168cd21527e6266d199ae9cf6e6fa
johnwilly990 <j.kim1940@gmail.com>
2023-01-05 20:47:06 -0500

    Add constant data for how it works section

--- src/constants/index.js
+++ src/constants/index.js
@@ -4,3 +4,30 @@ export const heroValues = {
     "ExTrack is a free expense tracking application. ExTrack allows you to enter and track your expenses in real-time, categorize them, and generate reports to understand their spending patterns. We aim to help you become more financially repsonsible, and viable, considering the current inflated market and economy!",
   cta: "Sign Up for Free!",
 };
+
+export const howItWorks = {
+  title: "How ExTrack Works",
+  description:
+    "Budgeting and expense tracking application can get complicated really quickly. With ExTrack, we ensure that your user experience is simple and smooth. We want you to focus your time and energy on making, and meeting, your financial goals rather than wasting it on figuring how to navigate and application",
+};
+
+export const works = [
+  {
+    id: 1,
+    title: "Four Expense Categories",
+    description:
+      "There are four categories of expense (Montly Payment, Flexible Expenses, Investments, and Savings). All of which you can modify.",
+  },
+  {
+    id: 2,
+    title: "Budget Remaining",
+    description:
+      "The budget displayed on your dashboard is the budget that you have to spend on flexible expenses after all fixed expenses (Monthly Payments, Investments, Savings) have been made from your income.",
+  },
+  {
+    id: 3,
+    title: "Real Time Update",
+    description:
+      "Whenever an update to your expenses happen, the budget remaining for the month is updated in real time.",
+  },
+];
