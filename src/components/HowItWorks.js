0d71f4a0ca1824fd84dc8180b64f7e323b948cb5
johnwilly990 <j.kim1940@gmail.com>
2023-01-05 20:46:46 -0500

    Complete how it works section

---
+++ src/components/HowItWorks.js
@@ -0,0 +1,17 @@
+const HowItWorks = ({ id, title, description }) => {
+  return (
+    <div className="flex flex-col items-start mx-4 mb-6">
+      <div className="rounded-l-full bg-babyGreen mb-8">
+        <div className="flex items-center space-x-2">
+          <div className="flex bg-green px-4 py-2 rounded-full text-white">
+            0{id}
+          </div>
+          <h3 className="pr-4">{title}</h3>
+        </div>
+      </div>
+      <p className="text-onyx md:mb-4">{description}</p>
+    </div>
+  );
+};
+
+export default HowItWorks;
--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,11 +1,35 @@
 import NavBar from "../components/NavBar";
 import Hero from "../components/Hero";
+import HowItWorks from "../components/HowItWorks";
+import { works } from "../constants";
+import { howItWorks } from "../constants";
+import time from "../assets/images/time.jpg";
 
 const LandingPage = () => {
   return (
     <section className="relative ">
       <NavBar />
       <Hero />
+      <div className="px-4 py-8 md:flex md:px-[32px] md:py-[48px] md:m-auto lg:px-[130px] lg:max-w-[1280px]">
+        <div className="md:flex md:flex-col md:w-1/2 md:pr-8">
+          <h2 className="text-2xl font-bold text-center mb-8 md:text-left">
+            {howItWorks.title}
+          </h2>
+          <p className="text-onyx mb-10 text-center md:text-left md:mt-[8px]">
+            {howItWorks.description}
+          </p>
+          <img
+            src={time}
+            alt="hourglass"
+            className="hidden md:block md:w-full rounded"
+          />
+        </div>
+        <div className="md:flex md:flex-col md:w-1/2">
+          {works.map((el) => (
+            <HowItWorks key={el.id} {...el} />
+          ))}
+        </div>
+      </div>
     </section>
   );
 };
