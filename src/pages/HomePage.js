093341e9defc84ea94ee1919e5db6c4105bd0e9c
johnwilly990 <j.kim1940@gmail.com>
2023-01-07 16:02:37 -0500

    Change variable name of constant data

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -3,12 +3,15 @@ import Footer from "../components/Footer/Footer";
 import Hero from "../components/Hero";
 import Reviews from "../components/Reviews/Reviews";
 import Differences from "../components/Differences/Differences";
+import { landingHero } from "../constants";
 
 const LandingPage = () => {
   return (
     <section className="relative ">
       <NavBar />
-      <Hero />
+      {landingHero.map((value, index) => (
+        <Hero key={index} {...value} />
+      ))}
       {/* How it works */}
       <Differences />
       {/* Reviews */}
