eeadb373624d97bc0abf56002eec17fa7c5fe790
johnwilly990 <j.kim1940@gmail.com>
2023-01-07 21:20:31 -0500

    Export cta button to its own component

--- src/components/Features/Features.js
+++ src/components/Features/Features.js
@@ -1,5 +1,6 @@
 import { features } from "../../constants";
 import Feature from "./Feature";
+import BtnCta from "../BtnCta";
 
 const Features = () => {
   return (
@@ -7,6 +8,9 @@ const Features = () => {
       {features.map((feature) => (
         <Feature key={feature.id} {...feature} />
       ))}
+      <div className="px-6 pb-8 flex justify-center bg-babyGreen">
+        <BtnCta cta="Sign Up Now!" />
+      </div>
     </>
   );
 };
--- src/components/Hero.js
+++ src/components/Hero.js
@@ -1,15 +1,17 @@
-import { Link } from "react-router-dom";
+import BtnCta from "./BtnCta";
 
 const Hero = ({ image, title, description, cta }) => {
   return (
-    <div className=" bg-babyGreen h-[33%]">
+    <div
+      className={`${cta === "Go To Features" ? "bg-white" : "bg-babyGreen"}`}
+    >
       <div className="flex flex-col px-4 py-8 md:flex-row-reverse md:justify-between md:px-[32px] md:py-[48px] md:m-auto lg:px-[130px] lg:max-w-[1280px]">
         <img
           src={image}
           alt="hero"
           className="w-full mb-6 md:w-[360px] md:mb-0 md:h-[272px] lg:w-[400px]"
         />
-        <div className="md:w-1/2 mr-8">
+        <div className="md:w-1/2 md:mr-8">
           <h1 className="text-2xl text-center font-bold mb-6 md:text-left">
             {title}
           </h1>
@@ -24,12 +26,7 @@ const Hero = ({ image, title, description, cta }) => {
               <p className="text-white my-3">{cta}</p>
             </a>
           ) : (
-            <Link
-              to="/signup"
-              className="flex justify-center items-center w-full rounded hover:bg-forestGreen bg-green md:w-[156px]"
-            >
-              <p className="text-white my-3">{cta}</p>
-            </Link>
+            <BtnCta cta={cta} />
           )}
         </div>
       </div>
