5fba170eda525eae5986cfc5b77f431e1659f021
johnwilly990 <j.kim1940@gmail.com>
2023-01-09 14:25:58 -0500

    Add conditional rendering for password visibility toggle which will be fixed later

--- src/components/Input.js
+++ src/components/Input.js
@@ -1,3 +1,6 @@
+import showPw from "../assets/icons/show-password.svg";
+import hidePw from "../assets/icons/hide-password.svg";
+
 const Input = ({
   type,
   placeholder,
@@ -7,18 +10,34 @@ const Input = ({
   inputError,
 }) => {
   return (
-    <input
-      type={type}
-      name={name}
-      placeholder={placeholder}
-      value={value}
-      onChange={handleInputChange}
-      className={` p-2 h-12 my-4 w-full border border-green solid rounded outline-forestGreen ${
-        name === "first_name" ? "md:mr-8" : ""
-      } ${
-        inputError && type === "password" ? "text-red-600 border-red-600" : ""
-      }`}
-    />
+    <>
+      <input
+        type={type}
+        name={name}
+        placeholder={placeholder}
+        value={value}
+        onChange={handleInputChange}
+        className={` p-2 h-12 my-4 w-full border border-green solid rounded outline-forestGreen ${
+          name === "first_name" ? "md:mr-8" : ""
+        } ${
+          inputError && type === "password" ? "text-red-600 border-red-600" : ""
+        }`}
+      />
+      {name === "password" && (
+        <img
+          src={showPw}
+          alt="password toggle"
+          className="absolute top-[62%] right-[10%]"
+        />
+      )}
+      {name === "confirm_password" && (
+        <img
+          src={showPw}
+          alt="password toggle"
+          className="absolute top-[71%] right-[10%]"
+        />
+      )}
+    </>
   );
 };
 
