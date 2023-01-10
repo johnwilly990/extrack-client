3e829f2349f7185e3dcfa5a920204ac26bcb3003
johnwilly990 <j.kim1940@gmail.com>
2023-01-09 21:15:10 -0500

    Refactor code to receive prop from its parent

--- src/components/Dashboard/DashboardValueCard.js
+++ src/components/Dashboard/DashboardValueCard.js
@@ -1,10 +1,4 @@
-const DashboardValueCard = ({
-  heading,
-  investment_amount,
-  saving_amount,
-  flexible_expense_amount,
-  recurring_amount,
-}) => {
+const DashboardValueCard = ({ heading, userData }) => {
   return (
     <div
       className={`flex flex-col items-center justify-center w-[45%] h-[120px] mb-6 gap-3 rounded py-4 shadow-xl md:h-[200px] lg:h-[180px] ${
@@ -16,20 +10,22 @@ const DashboardValueCard = ({
       <h3 className="md:text-lg">{heading}</h3>
       {heading === "Monthly Payments" && (
         <p className="md:text-lg">
-          ${recurring_amount?.toLocaleString("en-US")}
+          ${userData.recurring_amount?.toLocaleString("en-US")}
         </p>
       )}
       {heading === "Flexible Expenses" && (
         <p className="md:text-lg">
-          ${flexible_expense_amount?.toLocaleString("en-US")}
+          ${userData.flexible_expense_amount?.toLocaleString("en-US")}
         </p>
       )}
       {heading === "Savings" && (
-        <p className="md:text-lg">${saving_amount?.toLocaleString("en-US")}</p>
+        <p className="md:text-lg">
+          ${userData.saving_amount?.toLocaleString("en-US")}
+        </p>
       )}
       {heading === "Investments" && (
         <p className="md:text-lg">
-          ${investment_amount?.toLocaleString("en-US")}
+          ${userData.investment_amount?.toLocaleString("en-US")}
         </p>
       )}
     </div>
