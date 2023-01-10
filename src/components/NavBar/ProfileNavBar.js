315d3d06a3d32c33441f22c7c0fca8f2fe089ec4
johnwilly990 <j.kim1940@gmail.com>
2023-01-10 13:41:07 -0500

    Adjust width for large screens

--- src/components/NavBar/ProfileNavBar.js
+++ src/components/NavBar/ProfileNavBar.js
@@ -9,7 +9,7 @@ const ProfileNavBar = ({ registeredDate, setCurrentPage }) => {
   const [isActive, setIsActive] = useState(false);
 
   return (
-    <nav className={`bg-forestGreen ${isActive ? "md:w-1/3" : ""}`}>
+    <nav className={`bg-forestGreen lg:w-[20%] ${isActive ? "md:w-1/3" : ""}`}>
       <div className="flex justify-between items-center px-[14px] py-[13px] md:px-[32px] md:py-[20px] md:m-auto lg:hidden">
         <Link to="/dashboard">
           <img
