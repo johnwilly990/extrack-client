ed9436269a4f70a4ed1d4b5e7ed16d02867af7bb
johnwilly990 <j.kim1940@gmail.com>
2023-01-09 20:55:44 -0500

    Fix width of div

--- src/components/BtnCta/BtnCtaAdd.js
+++ src/components/BtnCta/BtnCtaAdd.js
@@ -4,7 +4,7 @@ const BtnCtaAdd = ({ text, onClick }) => {
   return (
     <div
       onClick={() => onClick((prevCheck) => !prevCheck)}
-      className="flex justify-center items-center cursor-pointer mb:16"
+      className="flex items-center w-[100px] cursor-pointer mb:16"
     >
       <img src={add} alt="add" className="w-[12px] h-[12px] mr-2" />
       <p className="text-sm text-green">Add {text}</p>
