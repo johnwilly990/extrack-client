b700ae8b4191351afd01e6f30bfd087d57611467
johnwilly990 <j.kim1940@gmail.com>
2023-01-10 02:31:11 -0500

    Add entries table, and its relative styles, for mobile and tablet viewports.

--- src/components/Expenses/ExpensesInfo.js
+++ src/components/Expenses/ExpensesInfo.js
@@ -1,9 +1,13 @@
 import { useState } from "react";
 import RemainingBalance from "../RemainingBalance";
-import ExpensesSelect from "./ExpensesSelect";
-import { months } from "../../constants";
-import edit from "../../assets/icons/edit.svg";
-import deleteIcon from "../../assets/icons/delete.svg";
+import ExpenseMonthSelect from "./ExpensesMonthSelect";
+import ExpensesCategorySelect from "./ExpensesCategorySelect";
+import ExpenseCategoryHeadings from "./ExpenseCategoryHeadings";
+import ExpensesHeading from "./ExpensesHeading";
+import BtnCtaAddEntry from "../BtnCta/BtnCtaAddEntry";
+import { months, expensesHeadings, dashboardValues } from "../../constants";
+import ExpenseEntry from "./ExpenseEntry";
+import ExpenseEntryMobile from "./ExpenseEntryMobile";
 
 const ExpensesInfo = ({ userData }) => {
   const date = new Date();
@@ -15,16 +19,40 @@ const ExpensesInfo = ({ userData }) => {
 
   return (
     <section>
-      <div className="flex flex-col justify-center items-center px-[48px] py-14">
+      <div className="flex flex-col justify-center items-center px-[48px] py-14 md:flex-row md:items-baseline">
         <RemainingBalance userData={userData} />
       </div>
-      <div className="flex flex-col items-center bg-babyGreen mb-8 md:hidden">
-        <ExpensesSelect
-          setCurrentCategory={setCurrentCategory}
+      <div className="flex flex-col items-center bg-babyGreen mb-8 md:flex-row md:px-8">
+        <ExpenseMonthSelect
           setCurrentMonth={setCurrentMonth}
           monthAtm={monthAtm}
           restOfMonths={restOfMonths}
         />
+        <ExpensesCategorySelect setCurrentCategory={setCurrentCategory} />
+        <div className="md:flex md:justify-between md:w-full">
+          {dashboardValues.map((heading, index) => (
+            <ExpenseCategoryHeadings
+              key={index}
+              {...heading}
+              currentCategory={currentCategory}
+              setCurrentCategory={setCurrentCategory}
+            />
+          ))}
+        </div>
+      </div>
+      <div className="hidden md:flex md:justify-end md:px-8 md:w-full md:mb-4">
+        <BtnCtaAddEntry text="Entry" />
+      </div>
+      <div className="flex justify-between bg-investmentGreen px-4 py-2 md:justify-start md:px-8">
+        {expensesHeadings.map((heading, index) => (
+          <ExpensesHeading key={index} heading={heading} />
+        ))}
+      </div>
+      <div className="bg-babyGreen px-4 py-2 md:px-8">
+        <ExpenseEntry />
+      </div>
+      <div className="bg-investments px-4 py-2 md:hidden">
+        <ExpenseEntryMobile />
       </div>
     </section>
   );
